eac95efc34c4057723bc28705e2d6f8b40ece64c
Dillon <Dillon.Zhang@smith.co>
2018-04-11 18:01:01 +0800

    add new request mapping

--- microservice.product.service/src/main/java/com/bdtv/ms/ecom/microservice/product/service/controller/ProductController.java
+++ microservice.product.service/src/main/java/com/bdtv/ms/ecom/microservice/product/service/controller/ProductController.java
@@ -5,12 +5,14 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bdtv.ms.ecom.microservice.product.service.entity.Product;
 import com.bdtv.ms.ecom.microservice.product.service.repository.ProductRepository;
 
 @RestController
+@RequestMapping("/product")
 public class ProductController {
 
 	@Autowired
