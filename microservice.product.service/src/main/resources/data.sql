cc72448d0aa72791816fcd8f966eb0c3be711b12
Dillon <Dillon.Zhang@smith.co>
2018-04-11 18:22:54 +0800

    add data and readme

---
+++ microservice.product.service/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into product (id, code, name, description) values (1, 'book', 'Think in Java', 'this is a very good java book');
+insert into product (id, code, name, description) values (2, 'pen', 'wumao', 'this is a good pen');
+insert into product (id, code, name, description) values (3, 'note book', 'haoyong', 'this is a great note book');
