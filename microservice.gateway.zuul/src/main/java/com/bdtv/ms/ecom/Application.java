36178bd10a4261a9a92e2f4b65fb7755b449f4ac
Walker_Liu <wliu@objectivasoftware.com>
2018-04-17 18:23:10 +0800

    Add gateway zuul

---
+++ microservice.gateway.zuul/src/main/java/com/bdtv/ms/ecom/Application.java
@@ -0,0 +1,15 @@
+package com.bdtv.ms.ecom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
