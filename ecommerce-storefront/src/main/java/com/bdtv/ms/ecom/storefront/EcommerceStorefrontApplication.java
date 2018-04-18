259a3b68a567283e218d88be6ffc1bcdd8b48396
Walker_Liu <wliu@objectivasoftware.com>
2018-04-18 18:19:37 +0800

    Add ecommerce storefront project

---
+++ ecommerce-storefront/src/main/java/com/bdtv/ms/ecom/storefront/EcommerceStorefrontApplication.java
@@ -0,0 +1,12 @@
+package com.bdtv.ms.ecom.storefront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceStorefrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceStorefrontApplication.class, args);
+	}
+}
