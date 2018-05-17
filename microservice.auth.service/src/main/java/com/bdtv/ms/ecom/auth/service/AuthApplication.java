8fe3caddd0ecff74ea014835d71b983d7d273bf7
Dillon <Dillon.Zhang@smith.co>
2018-05-17 18:39:55 +0800

    add Oauth support, still have an issue

--- microservice.auth.service/src/main/java/com/bdtv/ms/ecom/auth/service/AuthApplication.java
+++ microservice.auth.service/src/main/java/com/bdtv/ms/ecom/auth/service/AuthApplication.java
@@ -1,12 +1,15 @@
-package com.bdtv.ms.ecom.order.service;
+package com.bdtv.ms.ecom.auth.service;
+
+import java.security.Principal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpHeaders;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -14,8 +17,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableCircuitBreaker
-public class OrderApplication {
+@RestController
+public class AuthApplication {
 
     @Bean
     @LoadBalanced
@@ -24,7 +27,7 @@ public class OrderApplication {
     }
 
     public static void main(String[] args) {
-	SpringApplication.run(OrderApplication.class, args);
+	SpringApplication.run(AuthApplication.class, args);
     }
 
     @Bean
@@ -38,5 +41,10 @@ public class OrderApplication {
 			.exposedHeaders(HttpHeaders.SET_COOKIE).maxAge(3600L);
 	    }
 	};
+    }
+
+    @GetMapping("/user")
+    public Principal user(Principal user) {
+	return user;
     }
 }
